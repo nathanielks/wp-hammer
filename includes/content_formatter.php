2b2e45f9f48ae4effb9600f15e9eaa4f033acd76
Ivan Kruchkoff <ivan@kruchkoff.com>
2016-01-04 20:40:48 +0530

    Add Content formatters

---
+++ includes/content_formatter.php
@@ -0,0 +1,59 @@
+<?php
+namespace WP_CLI\Sweep;
+
+use WP_CLI;
+
+class content_formatter {
+
+	protected $dry_run;
+	protected $formatters;
+
+	function __construct( $formatters, $dry_run ) {
+		$this->dry_run = $dry_run;
+		$this->parse_formatters( $formatters );
+	}
+
+	function parse_formatters( $formatters ) {
+		$this->formatters = array();
+		foreach( $formatters as $formatter ) {
+			$options = explode( '.', $formatter, 2 );
+			var_dump( $options );
+			// Check we have table.column=type set
+			if ( 2 === count( $options ) ) {
+				$table = $options[0];
+				$generator_options = explode( '=', $options[1] );
+				$column = $generator_options[0];
+				$generator = isset( $generator_options[1] ) ? $generator_options[1] : null;
+				$this->add_formatter( $table, $column, $generator );
+			}
+		}
+	}
+
+	function add_formatter( $table, $column, $generator ) {
+		$formatters = is_array( $this->formatters ) ? $this->formatters : array();
+		$formatters[ $table ] = isset( $formatters[ $table ] ) && is_array( $formatters[ $table ] ) ? $formatters[ $table ] : array();
+		$formatters[ $table ][ $column ] = $generator;
+		$this->formatters = $formatters;
+	}
+
+	function run() {
+		do_action( 'wp_sweep_before_run_formatter' );
+		WP_CLI::line( "Running content formatters" );
+		foreach ( $this->formatters as $table => $formatters )     {
+			if ( $this->dry_run ) {
+				WP_CLI::line( "Dry run formatter $table" );
+			} else {
+				WP_CLI::line( "Running formatters for table: $table" );
+				do_action( 'wp_sweep_run_formatter_' . $table, $this->formatters );
+				$columns = array_keys( $formatters );
+				foreach ( $columns as $column ) {
+					do_action( 'wp_sweep_run_formatter_' . $table . '_' . $column , $formatters[ $column ] );
+				}
+			}
+
+		}
+	}
+}
+
+
+
--- wp-sweep.php
+++ wp-sweep.php
@@ -12,11 +12,12 @@ if ( ! defined('WP_CLI') || ! WP_CLI ) {
 	return;
 }
 
-$pruners = glob( __DIR__ . '/includes/pruners/*.php');
-
-foreach ( $pruners as $pruner ) {
-	require_once $pruner;
-}
+$content_manipulators = glob( __DIR__ . '/includes/{pruners,formatters,generators}/*.php', GLOB_BRACE);
 
 require_once 'autoload.php';
+
+foreach ( $content_manipulators as $content_manipulator ) {
+	require_once $content_manipulator;
+}
+
 WP_CLI::add_command( 'sweep', 'WP_CLI\Sweep\command' );
