d514bd5d0d57ef78aa0c2d68260849a18efbaf12
Ivan Kruchkoff <ivan@kruchkoff.com>
2016-01-04 12:50:20 +0530

    Doc blocks + actions/filters

--- includes/wp-cli-sweep.php
+++ includes/wp-cli-sweep.php
@@ -12,12 +12,24 @@ class WP_Sweep extends WP_CLI_Command {
 	protected $dry_run = false;
 
 
+	/**
+	 * Call with wp sweep, no need for additional commands, only parameters per README
+	 * @param $args
+	 * @param $assoc_args
+	 */
 	function __invoke( $args, $assoc_args ) {
+		do_action( 'wp_sweep_before_parse_arguments', $args, $assoc_args );
 		$this->parse_arguments( $args, $assoc_args );
+		do_action( 'wp_sweep_after_parse_arguments', $args, $assoc_args );
 		$this->run();
 
 	}
 
+	/**
+	 * Arguments parser for all supplied arguments
+	 * @param $args
+	 * @param $assoc_args
+	 */
 	function parse_arguments( $args, $assoc_args ) {
 		while ( count( $args ) ) {
 			$arg = array_shift( $args );
@@ -37,10 +49,19 @@ class WP_Sweep extends WP_CLI_Command {
 		$this->dry_run = ! empty( $assoc_args[ 'dry-run' ] );
 	}
 
+	/**
+	 * Parse an arg for an individual property, if it exists.
+	 *
+	 * @param $args
+	 * @param $property
+	 *
+	 * @return mixed
+	 */
 	function parse_argument( $args, $property ) {
+		do_action( 'wp_sweep_before_parse_argument_' . $property, $args );
 		if ( property_exists( $this, $property ) && count( $args ) && '-' !== substr( $args[0], 0, 1 ) ) {
 			$arg_values = explode( ',', array_shift( $args ) );
-			$this->{ "$property" } = array_unique( array_merge_recursive( (array) $this->{ "$property" }, $arg_values ) );
+			$this->{ "$property" } = apply_filters( 'wp_sweep_argument_' . $property, array_unique( array_merge_recursive( (array) $this->{ "$property" }, $arg_values ) ) );
 		}
 		return $this->{ "$property" };
 
@@ -53,6 +74,7 @@ class WP_Sweep extends WP_CLI_Command {
 		var_dump( $this->formats );
 		var_dump( $this->limits );
 		var_dump( $this->dry_run );
+		var_dump( get_editable_roles() );
 	}
 }
 
