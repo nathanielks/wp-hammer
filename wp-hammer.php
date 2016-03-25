460ba66356f652952b3806b42e19ab16fab88261
Ivan Kruchkoff <ivan@kruchkoff.com>
2016-03-25 15:47:16 -0300

    Don't load the autoloader if we're installed via package

--- wp-hammer.php
+++ wp-hammer.php
@@ -12,7 +12,14 @@ if ( ! defined('WP_CLI') || ! WP_CLI ) {
 	return;
 }
 
-require_once 'vendor/autoload.php';
+// If you install via package, the autoloader is already included and doesn't live in the root folder.
+if ( ! class_exists('WP_CLI\Hammer\Command') ) {
+	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		require_once __DIR__ . '/vendor/autoload.php';
+	} else {
+		die( "Please, run composer install first" );
+	}
+}
 
 WP_CLI::add_command( 'ha', 'WP_CLI\Hammer\Command' );
 WP_CLI::add_command( 'hammer', 'WP_CLI\Hammer\Command' );
