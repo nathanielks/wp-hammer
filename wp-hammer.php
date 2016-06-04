b217967c9d4a9eec07c9d66e724e5098057f4061
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2016-06-04 16:05:19 -0700

    search for autoload.php in base composer vendor directory

--- wp-hammer.php
+++ wp-hammer.php
@@ -15,7 +15,10 @@ if ( ! defined('WP_CLI') || ! WP_CLI ) {
 // If you install via package, the autoloader is already included and doesn't live in the root folder.
 if ( ! class_exists('WP_CLI\Hammer\Command') ) {
 	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-		require_once __DIR__ . '/vendor/autoload.php';
+        require_once __DIR__ . '/vendor/autoload.php';
+    // Search in base composer vendor directory
+    } else if ( file_exists( __DIR__ . '/../../autoload.php' ) ) {
+        require_once __DIR__ . '/../../autoload.php';
 	} else {
 		WP_CLI::error( "Please, run composer install first" );
 	}
